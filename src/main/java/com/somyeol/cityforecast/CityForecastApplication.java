79fb6a196ede60e19e6c11b77b2a4b4a93f83835
hskhsmm <hskhsmm@naver.com>
2025-05-12 16:45:10 +0900

    fix: README 수정

--- src/main/java/com/somyeol/cityforecast/CityForecastApplication.java
+++ src/main/java/com/somyeol/cityforecast/CityForecastApplication.java
@@ -2,8 +2,10 @@ package com.somyeol.cityforecast;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CityForecastApplication {
 
 	public static void main(String[] args) {
